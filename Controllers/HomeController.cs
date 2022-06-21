88d10e539efb2b6c82d359d4f69818a4898d22f2
ujAdmin <Admin@CHETUIWK324>
2022-06-21 13:50:56 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using DurgeshCoreAPI.Data;
+using DurgeshCoreAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DurgeshCoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public HomeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [Route("Get/Emplist/all")]
+        [HttpGet]
+        public List<Employee> ShowList()
+        {
+            var res = _db.Employees.ToList();
+            return res;
+        }
+
+    }
+}
